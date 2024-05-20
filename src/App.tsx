a8de89563bca5840bf39402e69c243f33c6c9f74
SedovAlexandr <sedovaleksandr808@gmail.com>
2024-05-20 10:01:36 +0300

    сделал пагинацию

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import Search from './components/Search'
 import SearchResults from './components/SearchResults'
@@ -9,29 +9,40 @@ function App() {
 
   const [publications, setPublications] = useState<Publication[]>()
 
+  const [serverUrl, setServerUrl] = useState<string>('http://localhost:5000/api/publication/publications?limit=10&api-version=1.0');
+
+  useEffect(() => {
+    fetch(serverUrl)
+      .then((response) => {
+        if (response.ok) return response.json()
+        throw new Error('Request failed.')
+      })
+      .then((json) => {
+        setPublications( json );
+      })
+      .catch((error) => {
+        console.log(error)
+        
+      })
+  },[serverUrl]);
+
   return (
     <>
       <Search searchAction={(event, request) => {
-        let requestString = 'http://localhost:5000/api/publication/publications?'
+        let requestString = 'http://localhost:5000/api/publication/publications?limit=10&api-version=1.0'
 
         request.map( r => r.value != "" ? 
-           requestString = requestString.concat(r.criteriaValue + "=" + r.value + "&") : 1)
+           requestString = requestString.concat("&" + r.criteriaValue + "=" + r.value) : 1)
 
            console.log(requestString)
 
-        fetch(requestString + "limit=10&api-version=1.0")
-        .then((response) => {
-          if (response.ok) return response.json()
-          throw new Error('Request failed.')
-        })
-        .then((json) => {
-          setPublications( json );
-        })
-        .catch((error) => {
-          console.log(error)
-        })
+        setServerUrl(requestString)
+        
       }}/>
-      <SearchResults publications={publications}/>
+      <SearchResults publications={publications} changePageAction={(skip) => {
+          const urlTmp = serverUrl.replace(/&skip=\d+/g, "");
+          setServerUrl(urlTmp + '&skip=' + skip)
+        }}/>
       {/* <Routes>
             <Route path="/" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -6,10 +6,11 @@ import { Publication } from '../types'
 import PublicationCard from './PublicationCard'
 
 type Props = {
-  publications: Publication[] | undefined
+  publications: Publication[] | undefined,
+  changePageAction: (skip: number) => void
 }
 
-function SearchResults({publications} : Props) {
+function SearchResults({publications, changePageAction} : Props) {
 
   const [pageNumber, setPageNumber] = useState<number>(1);
 
@@ -18,7 +19,7 @@ function SearchResults({publications} : Props) {
       <p className='head2'>Результаты поиска:</p>
       
       {
-        publications == undefined ?  
+        publications == undefined || publications.length == 0 ?  
         <p>ничего не нашлось</p>
         : publications.map( p => (
           <PublicationCard publication={p}/>
@@ -26,9 +27,19 @@ function SearchResults({publications} : Props) {
       }
 
       <div className='Pagination'>
-        <button className='previous'><Previous/></button>
+        <button className='previous' onClick={() => {
+          if(pageNumber != 1) {
+            changePageAction((pageNumber - 2)*10)
+            setPageNumber(pageNumber - 1)
+          }
+        }}><Previous/></button>
         <b>{pageNumber}</b>
-        <button className='next' ><Next/></button>
+        <button className='next' onClick={() => {
+          if(publications != undefined && publications.length == 10) {
+            changePageAction((pageNumber)*10)
+            setPageNumber(pageNumber + 1)
+          }
+        }}><Next/></button>
       </div>
     </>
   )
